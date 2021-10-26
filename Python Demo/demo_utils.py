cc08bb3d13c293ce418146add6b81ed82201767f
Lyle Regenwetter <Lyleregenwetter@gmail.com>
2021-10-26 19:53:35 -0400

    python demo updates

---
+++ Python Demo/demo_utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 20 09:38:12 2021
+
+@author: Lyleregenwetter
+"""
+
+
+def print_numbers():
+    count=0
+    while count<3:
+        if count==2:
+            print(count)
+        count+=1
+    return True
+
+print(print_numbers())
+
+# def print_numbers():
+#     count = 0
+#     while count<3:
+#         if count!=2:
+#             print(count)
+#         count+=1
+#     return True
+# print_numbers()
